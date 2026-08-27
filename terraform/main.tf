195cc7bced44682d625413c7f167982ddb3aded2
CG3-C0ding-W <curtis.garrett.iii@gmail.com>
2026-08-26 23:45:07 -0700

    Terraform updates

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,30 +1,50 @@
 
 terraform {
-    required_providers {
-        kind        = { source = "tehcyx/kind", version = "~> 0.5"}
-        helm        = { source = "hashicorp/helm", version = "~> 2.14"}
-        kubernetes  = { source = "hashicorp/kubernetes", version = "~> 2.31"}
-    }
+  required_providers {
+    kind       = { source = "tehcyx/kind", version = "~> 0.5" }
+    helm       = { source = "hashicorp/helm", version = "~> 3.0" }
+    kubernetes = { source = "hashicorp/kubernetes", version = "~> 2.31" }
+  }
 }
 
+provider "kind" {}
+
 resource "kind_cluster" "this" {
-    name            = "gitops"
-    wait_for_ready  = true
-    kind_config     = file("${path.module}/kind-config.yaml")
+  name           = "gitops"
+  wait_for_ready = true
+
+  kind_config {
+    kind        = "Cluster"
+    api_version = "kind.x-k8s.io/v1alpha4"
+
+    node {
+      role = "control-plane"
+
+      kubeadm_config_patches = [
+        "kind: InitConfiguration\nnodeRegistration:\n  kubeletExtraArgs:\n    node-labels: \"ingress-ready=true\"\n"
+      ]
+
+      extra_port_mappings {
+        container_port = 80
+        host_port      = 80
+        protocol       = "TCP"
+      }
+    }
+  }
 }
 
 provider "helm" {
-    kubernetes = {
-        host                    = kind_cluster.this.endpoint
-        client_certificate      = kind_cluster.this.client_certificate
-        client_key              = kind_cluster.this.client_key
-        cluster_ca_certificate  = kind_cluster.this.cluster_ca_certificate
-    }
+  kubernetes = {
+    host                   = kind_cluster.this.endpoint
+    client_certificate     = kind_cluster.this.client_certificate
+    client_key             = kind_cluster.this.client_key
+    cluster_ca_certificate = kind_cluster.this.cluster_ca_certificate
+  }
 }
 
 provider "kubernetes" {
-    host                    = kind_cluster.this.endpoint
-    client_certificate      = kind_cluster.this.client_certificate
-    client_key              = kind_cluster.this.client_key
-    cluster_ca_certificate  = kind_cluster.this.cluster_ca_certificate
+  host                   = kind_cluster.this.endpoint
+  client_certificate     = kind_cluster.this.client_certificate
+  client_key             = kind_cluster.this.client_key
+  cluster_ca_certificate = kind_cluster.this.cluster_ca_certificate
 }
