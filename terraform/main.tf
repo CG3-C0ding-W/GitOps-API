b1c46d9a3777572b04f926377e2fb89d4b71aed8
CG3-C0ding-W <curtis.garrett.iii@gmail.com>
2026-08-26 23:12:08 -0700

    Action: Completed fronted and started IaC construction

---
+++ terraform/main.tf
@@ -0,0 +1,30 @@
+
+terraform {
+    required_providers {
+        kind        = { source = "tehcyx/kind", version = "~> 0.5"}
+        helm        = { source = "hashicorp/helm", version = "~> 2.14"}
+        kubernetes  = { source = "hashicorp/kubernetes", version = "~> 2.31"}
+    }
+}
+
+resource "kind){
+    name            = "gitops"
+    wait_for_ready  = true
+    kind_config     = file("${path.module}/kind-config.yaml
+}
+
+provider "helm" {
+    kubernetes {
+        host                    = kind_cluster.this.endpoint
+        client_certificate      = kind_cluster.this.client_certificate
+        client_key              = kind_cluster.this.client_key
+        cluster_ca_certificate  = kind_cluster.this.cluster_ca_certificate
+    }
+}
+
+provider "kubernetes" {
+    host                    = kind_cluster.this.endpoint
+    client_certificate      = kind_cluster.this.client_certificate
+    client_key              = kind_cluster.this.client_key
+    cluster_ca_certificate  = kind_cluster.this.cluster_ca_certificate
+}
